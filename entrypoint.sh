8ff3ccd6199dc1f128d9fe6d69d08a74566c28bb
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2023-09-02 22:22:17 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "================================="
 echo "=== Linting OpenApi spec file ==="
 echo "================================="
 
-cd /github/workspace/ && swagger-cli validate "$INPUT_FILE_PATH"
+swagger-cli validate "$INPUT_FILE_PATH"
